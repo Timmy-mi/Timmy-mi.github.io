fec7c91eba2ae64e7db8f8985e0d1cdb56356321
Timmy-mi <shenshutian18@gmail.com>
2022-04-03 10:58:09 -0400

    Create Gold.js

---
+++ Gold.js
@@ -0,0 +1,107 @@
+function acd(){
+	alert('bruh why do you have to do this man');
+}
+function today(){
+	return document.getElementById('now').innerHTML = Date();
+}
+function papa(){
+	return document.getElementById('im').src = 'https://images.gnwcdn.com/2020/articles/2020-07-19-11-39/minecraft-fans-find-seed-for-title-screen-background-panorama-1595155174941.jpg/EG11/resize/1200x-1/minecraft-fans-find-seed-for-title-screen-background-panorama-1595155174941.jpg';
+}
+function para(){
+	return document.getElementById('pp').innerHTML = "Vincent Wang has memory diseases";
+}
+function vw(){
+	var a = document.getElementById('three')
+	a.style.backgroundColor = 'cyan';
+	a.style.textAlign = 'center';
+	a.style.fontFamily = 'arial';
+}
+function taigaole(e100){
+	var vincent = (e100-32)*5/9
+	document.getElementById('convert').innerHTML = vincent;
+
+}
+/*function res(){
+	document.getElementById('lalal').innerHTML = 10 + 10;
+}*/
+
+var one = parseFloat(prompt('input a negative value I beg you'));
+var two = parseFloat(prompt('input a positive value I beg you sir'));
+function out(){
+	document.getElementById('sum').innerHTML = one + two;
+}
+
+var a = window.prompt('tell me your first name NOW');
+var b = window.prompt('TELL ME YOUR LAST NAME');
+var e = a.charAt(0);
+var d = b.charAt(0);
+
+function idareyou(){
+	var c = "Nice to meet you " + e.toUpperCase() + a.slice(1) + " " + d.toUpperCase() + b.slice(1);
+	return document.getElementById('last').innerHTML = c;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var a = window.prompt('input value for the number of trials');
+var b = window.prompt('input the probability of sucess');
+var c = 1-b;
+
+function mother(){
+	var i = 0;
+	var d = a - i;
+
+	while (i<=a+1){
+		i++
+		var final = 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+*/
