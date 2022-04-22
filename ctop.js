3510cb4686a2a0934ac5d52b05c615d2f6946951
Timmy-mi <shenshutian18@gmail.com>
2022-04-21 23:23:25 -0400

    Create ctop.js

---
+++ ctop.js
@@ -0,0 +1,27 @@
+//Yifei li JS
+//index JS
+//colunm pic change
+function change(){
+	document.getElementById('cimg').src  = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/640px-Python-logo-notext.svg.png"
+} 
+//big pig change 
+function change2(){
+	document.getElementById('img2').src = "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/687413/retina_1708x683_cover-0402-an-introduction-to-mocking-in-python-Waldek_Newsletter-fdc7cce1eda95aee17e25b769a2bd183.png"
+}
+//games pic to apps pic
+function change6(){
+	document.getElementById('c5').src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/1200px-Adobe_Photoshop_CC_icon.svg.png";
+	document.getElementById('c7').src = "https://upload.wikimedia.org/wikipedia/en/0/05/Call_of_Duty_Black_Ops_II_box_artwork.png";
+	document.getElementById('c6').src = "https://upload.wikimedia.org/wikipedia/commons/c/c7/Windows_logo_-_2012.png";
+	document.getElementById('c9').src = "https://cdn-wglc.gcdn.co/uploads/public/layouts/2018_10_24__12_09_37_WoWS_General_Invit_NA/variative_content/EN/logo/elements/warships/elements/img/2681606/logo-warships_1447933259.png";
+	document.getElementById('c8').src = "https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png";
+	document.getElementById('c10').src = "https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png";
+}
+//Contact us js
+/*Name*/
+function pop(){
+	var fN = prompt("what is your full name?"); 
+	document.getElementById('wname').innerHTML = "Hi"+ " " + fN +" "+"Please fill in the following page so that we could discuss more together.";
+	document.getElementById('dis').style.display = "none";
+}
+//End of Yifei JS
